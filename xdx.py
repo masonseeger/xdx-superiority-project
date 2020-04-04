22a068683aa25467760e438f8fbaa6af7c40d56b
jimtheplant <55642944+jimtheplant@users.noreply.github.com>
2020-04-03 20:47:24 -0500

    Stickin it to Mason

--- xdx.py
+++ xdx.py
@@ -5,16 +5,17 @@ import sys
 from random import randint as randy
 
 def roll_baby_roll(left, size):
-    if left < 1:
-        return 0
-    elif left == 1:
-        return randy(1, size)
-    else:
-        return roll_baby_roll(left-1, size) + randy(1, size)
+	for i in range(left):
+		yield randy(1,size)
+
 
 def main():
     dice = sys.argv[1].split('d')
-    print(roll_baby_roll(int(dice[0]), int(dice[1])))
+    tot = 0
+    for dice in roll_baby_roll(int(dice[0]), int(dice[1])):
+        print(dice)
+        tot += dice
+    print("Sum: ", tot)
 
 if __name__ == "__main__":
     main()
